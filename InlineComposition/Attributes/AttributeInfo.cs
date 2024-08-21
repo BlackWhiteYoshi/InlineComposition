3f61bb94aabf66d924f9d051b00984eeb980a5cf
BlackWhiteYoshi <bastian@firerocket.de>
2024-08-21 17:49:40 +0200

    upgraded to version 1.3.0

--- InlineComposition/Attributes/AttributeInfo.cs
+++ InlineComposition/Attributes/AttributeInfo.cs
@@ -2,5 +2,5 @@
 
 public static partial class Attributes {
     private const string NAME = "InlineComposition";
-    private const string VERSION = "1.2.0";
+    private const string VERSION = "1.3.0";
 }
